f07f38a54bab4923d7b64d0012a6f7eb2f17fa48
douglasswm <douglasswm@gmail.com>
2020-03-15 13:51:00 +0800

    fix: chromedriver

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ def job():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=options)
-    driver2 = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=options)
+    driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=options)
+    driver2 = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=options)
 
     # CALL BINANCE URL
     driver.get(binance_url)
